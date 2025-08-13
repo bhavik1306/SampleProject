f5d212962159769474c2ec1bf20242fbfd99c93d
bhavikhumancloud <bhavik.mohod@humancloud.co.in>
2025-08-13 14:50:31 +0530

    Adding new code

--- SampleBddCucumberProject/src/test/java/stepdefinitions/LoginStepDefs.java
+++ SampleBddCucumberProject/src/test/java/stepdefinitions/LoginStepDefs.java
@@ -1,22 +1,62 @@
 package stepdefinitions;
 
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.After;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.SQLOutput;
+import java.sql.Time;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 
 public class LoginStepDefs {
+    WebDriver driver;
+    @After
+    public void tearDown(){
+        driver.close();
+    }
     @Given("user is on login page")
-    public void user_on_login_page() {
-        System.out.println("User on login page");
+    public void user_on_login_page() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://leaseoasis.ae/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        System.out.println("title of the page: "+driver.getTitle());
+        Assert.assertTrue(driver.getTitle().toString().contains("Lease Oasis"));
     }
 
     @When("user enters valid credentials")
-    public void user_enters_valid_credentials() {
-        System.out.println("User enters credentials");
+    public void user_enters_valid_credentials(DataTable dataTable) throws InterruptedException {
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        List<Map<String,String>> table = dataTable.asMaps(String.class,String.class);
+
+        driver.findElement(By.xpath("//*[text()='Log In']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[text()='Operator']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@autocomplete='email']")).sendKeys(table.get(0).get("Username"));
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@autocomplete='current-password']")).sendKeys(table.get(0).get("Password"));
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        Thread.sleep(2000);
     }
 
     @Then("user is redirected to the homepage")
-    public void user_redirected_homepage() {
-        System.out.println("User redirected");
+    public void user_redirected_homepage() throws InterruptedException {
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        System.out.println("title of the page: " + driver.getTitle());
+        Thread.sleep(3000);
     }
+
 }

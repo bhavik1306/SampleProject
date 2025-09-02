724ae5ab9346edf867bf0cb0b142d7f4a952cf23
bhavikhumancloud <bhavik.mohod@humancloud.co.in>
2025-09-02 16:30:37 +0530

    changes did in pom file. added plugin

--- SampleBddCucumberProject/src/test/java/runners/TestRunner.java
+++ SampleBddCucumberProject/src/test/java/runners/TestRunner.java
@@ -3,15 +3,17 @@ package runners;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"stepdefinitions"},
-        plugin = {"pretty", "html:target/cucumber-reports.html"},
+//        plugin = {"pretty", "html:target/cucumber-reports.html"},
+        plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
         monochrome = true
 )
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
